52f70c0e08fc30b765ad62e08edb9eff0ee9df2e
Dominik Aumayr <dominik@aumayr.name>
2014-12-02 12:36:48 +0100

    First dummy view, urls

--- src/stupa/stupa/settings.py
+++ src/stupa/stupa/settings.py
@@ -30,12 +30,14 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'djamin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'vote'
 )
 
 MIDDLEWARE_CLASSES = (
