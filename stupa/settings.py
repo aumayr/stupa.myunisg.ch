776ddc2acfa6fe327dabf4585a4ac1e75fb1c6ff
Dominik Aumayr <dominik@aumayr.name>
2014-12-09 09:22:05 +0100

    Restructuring for heroku, dev-settings

--- stupa/settings.py
+++ stupa/settings.py
@@ -17,12 +17,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'g)s!813=uz+zu(&s1+)599ee^24t^ppvd&^isaz@227mq^somn'
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-TEMPLATE_DEBUG = True
+TEMPLATE_DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -58,12 +58,12 @@ WSGI_APPLICATION = 'stupa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
---
+++ stupa/settings_dev.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+from .settings import *
+
+DEBUG = True
+
+TEMPLATE_DEBUG = DEBUG
+
+DATABASES['default'] =  {
+                            'ENGINE': 'django.db.backends.sqlite3',
+                            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+                        }
